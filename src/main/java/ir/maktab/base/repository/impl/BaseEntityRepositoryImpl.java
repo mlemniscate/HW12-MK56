787f518c644555047faf5371d3f062931998d903
Milad Abdi <miladabdi1374@gmail.com>
2021-08-28 11:46:36 +0430

    Add BaseEntityRepository and MainApp, modify BaseEntity.

---
+++ src/main/java/ir/maktab/base/repository/impl/BaseEntityRepositoryImpl.java
@@ -0,0 +1,61 @@
+package ir.maktab.base.repository.impl;
+
+import ir.maktab.base.domain.BaseEntity;
+import ir.maktab.base.repository.BaseEntityRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.io.Serializable;
+import java.util.List;
+
+public abstract class BaseEntityRepositoryImpl<E extends BaseEntity<ID>, ID extends Serializable> implements BaseEntityRepository<E, ID> {
+
+    protected final EntityManager entityManager;
+
+    public BaseEntityRepositoryImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public abstract Class<E> getEntityClass();
+
+    @Override
+    public E save(E e) {
+        if(e.getId() == null) {
+            entityManager.persist(e);
+            return e;
+        } else {
+            return entityManager.merge(e);
+        }
+    }
+
+    @Override
+    public E findById(ID id) {
+        return null;
+    }
+
+    @Override
+    public List<E> findAll() {
+        return null;
+    }
+
+    @Override
+    public void delete(E e) {
+
+    }
+
+    @Override
+    public boolean existsById(ID id) {
+        return false;
+    }
+
+    @Override
+    public Long countAll() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Long> criteriaQuery = criteriaBuilder.createQuery(Long.class);
+        Root<E> root = criteriaQuery.from(getEntityClass());
+        criteriaQuery.select(criteriaBuilder.count(root));
+        return entityManager.createQuery(criteriaQuery).getSingleResult();
+    }
+}
